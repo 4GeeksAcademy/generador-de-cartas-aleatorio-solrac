389030f36a2af49521139ae0e8eba11d751ce106
Carlos Enrique Contreras Duque <carlos.contredu@gmail.com>
2025-07-17 10:30:43 +0000

    falta la logica

--- src/app.js
+++ src/app.js
@@ -5,7 +5,27 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function () {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const values = ['A', '2', '3', '4', '5', '6', '7', '8', '9', 'J', 'Q', 'K'];
+  const symbols = ["♦", "♥", "♠", "♣"];
+  //const colorHints = ["danger", "black"];
+
+  const randomValues = values[Math.floor(Math.random() * values.length)];
+  const randomSymbols = symbols[Math.floor(Math.random() * symbols.length)];
+  const randomColor = randomSymbols[Math.floor(Math.random() * colorHints.length)];
+
+
+
+  const otherCard = () => {
+
+  }
+
+  document.getElementById("palo-superior").innerHTML = randomSymbols
+  document.getElementById("valor-carta").innerHTML = randomValues
+  document.getElementById("palo-inferior").innerHTML = randomSymbols
+
+
+
 };
+
