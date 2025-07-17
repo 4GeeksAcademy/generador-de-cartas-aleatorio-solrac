8722f600a3e90310a3e53b3e5df397089c7fdd7c
Carlos Enrique Contreras Duque <carlos.contredu@gmail.com>
2025-07-17 19:18:49 +0000

    terminado

--- src/app.js
+++ src/app.js
@@ -9,23 +9,42 @@ window.onload = function () {
   //write your code here
   const values = ['A', '2', '3', '4', '5', '6', '7', '8', '9', 'J', 'Q', 'K'];
   const symbols = ["♦", "♥", "♠", "♣"];
-  //const colorHints = ["danger", "black"];
 
-  const randomValues = values[Math.floor(Math.random() * values.length)];
-  const randomSymbols = symbols[Math.floor(Math.random() * symbols.length)];
-  const randomColor = randomSymbols[Math.floor(Math.random() * colorHints.length)];
+  const randomCard = () => {
 
+    const randomValues = values[Math.floor(Math.random() * values.length)];
+    const randomSymbols = symbols[Math.floor(Math.random() * symbols.length)];
 
+    document.getElementById("palo-superior").innerHTML = randomSymbols
+    document.getElementById("valor-carta").innerHTML = randomValues
+    document.getElementById("palo-inferior").innerHTML = randomSymbols
 
-  const otherCard = () => {
+    if (randomSymbols === "♦" || randomSymbols === "♥") {
+      document.getElementById("palo-superior").style.color = "red";
+      document.getElementById("palo-inferior").style.color = "red";
+    }
+    else {
+      document.getElementById("palo-superior").style.color = "black";
+      document.getElementById("palo-inferior").style.color = "black";
+    }
 
   }
 
-  document.getElementById("palo-superior").innerHTML = randomSymbols
-  document.getElementById("valor-carta").innerHTML = randomValues
-  document.getElementById("palo-inferior").innerHTML = randomSymbols
+  randomCard();
+
+  document.getElementById("myButton").addEventListener("click", randomCard);
+
+  const otherCard = () => {
+    const myButton = document.getElementById("myButton");
+    myButton.addEventListener("click", randomCard);
+
+  };
+
+  otherCard();
+
+  setInterval(randomCard, 3000); // Cambia la carta cada 3 segundos
+
+}
 
 
-
-};
 
